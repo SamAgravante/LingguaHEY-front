546dabe052e1b35a758725cc28300eb1f27bcb85
InFinacity <yevrahznah@yahoo.com>
2025-04-09 20:39:22 +0800

    Profile partial design

--- lingguahey/src/components/Auth/Login.jsx
+++ lingguahey/src/components/Auth/Login.jsx
@@ -1,18 +1,36 @@
 // src/components/Auth/Login.jsx
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { login } from "../../firebase/auth";
 import { useNavigate } from "react-router-dom";
+import {
+  Grid,
+  Stack,
+  Box,
+  Typography,
+  Button,
+  TextField,
+  InputAdornment,
+  IconButton
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
   const [form, setForm] = useState({ email: "", password: "" });
   const [error, setError] = useState("");
+  const emailRef = useRef();
+  const passwordRef = useRef();
   const navigate = useNavigate();
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
+    console.log(form.email,form.password);
     e.preventDefault();
     setError("");
     try {
@@ -23,16 +41,92 @@ const Login = () => {
     }
   };
 
-  return (
-    <div>
-      <h2>Login</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input name="email" placeholder="Email" type="email" onChange={handleChange} required />
-        <input name="password" placeholder="Password" type="password" onChange={handleChange} required />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    return (
+      <Grid
+      container
+      sx={{
+          backgroundColor: '#e2a5bf',
+          minHeight: '100vh',
+          minWidth: '100vw',
+          display: 'flex',
+          justifyContent: 'center',
+          //alignItems: 'center',
+            }}
+        >
+      <Stack direction="column" alignItems="center" >
+          <Box
+              sx={{
+                  backgroundColor: '#D2E0D3',
+                  minHeight: '50vh',
+                  minWidth: '30vw',
+                  borderBottomLeftRadius: '50px',
+                  borderBottomRightRadius: '50px',
+              }}>
+
+          </Box>
+          <Typography 
+            variant="h4"
+            paddingTop={2}
+            paddingBottom={2}>
+            Log In
+          </Typography>
+
+          <TextField
+            inputRef={emailRef}
+            label="Email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            variant="outlined"
+            placeholder="Enter Email"
+            fullWidth
+            sx={{ mb: 2 }}
+          />
+
+          <TextField
+            inputRef={passwordRef}
+            label="Password"
+            name="password"
+            value={form.password}
+            onChange={handleChange}
+            type={showPassword ? "text" : "password"}
+            variant="outlined"
+            placeholder="Enter Password"
+            fullWidth
+            sx={{ mb: 2 }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label={
+                      showPassword ? "Hide password" : "Show password"
+                    }
+                    onClick={handleClickShowPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ minWidth: "100%", borderRadius: "20px" }}
+            onClick={handleSubmit}
+          >
+            Log in
+          </Button>
+
+          {error && (
+            <Typography color="error" mt={2}>
+              {error}
+            </Typography>
+          )}
+      </Stack>
+    </Grid>
   );
 };
 
