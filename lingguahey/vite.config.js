5e8bb2fad616c34436cc5919adc03dea3697661f
InFinacity <yevrahznah@yahoo.com>
2025-04-12 11:41:16 +0800

    experimental stuff
    env

--- lingguahey/vite.config.js
+++ lingguahey/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< Updated upstream
+=======
+  base: process.env.VITE_BASE_PATH || "/"
+  //base: import.meta.env.VITE_BASE_PATH || "/"
+  //base: process.env.VITE_BASE_PATH || "/LingguaHEY-front"
+>>>>>>> Stashed changes
 })
